997963fb07dce2ac9f74e3439d06666093e86ad3
Thomas Soete <github@alkorin.fr>
2015-01-12 22:52:49 +0100

    Add Decode sub to websafebase64

--- websafebase64/websafebase64.go
+++ websafebase64/websafebase64.go
@@ -6,3 +6,13 @@ import "encoding/base64"
 func Encode(data []byte) string {
 	return strings.Trim(base64.URLEncoding.EncodeToString(data), "=")
 }
+
+func Decode(data string) ([]byte, error) {
+
+	// Websafe base64 doesn't have padding, append it (len should be %4)
+	if pad := 4 - len(data)%4; pad != 4 {
+		data += strings.Repeat("=", pad)
+	}
+
+	return base64.URLEncoding.DecodeString(data)
+}
