9fc76fb9ec9aa490850dcbdf2985ab86c4b9c32d
Thomas Soete <github@alkorin.fr>
2015-01-18 16:41:27 +0100

    Add comments on websafebase64

--- websafebase64/websafebase64.go
+++ websafebase64/websafebase64.go
@@ -3,10 +3,12 @@ package websafebase64
 import "strings"
 import "encoding/base64"
 
+// Encode data using websafe-base64 without '=' padding
 func Encode(data []byte) string {
 	return strings.Trim(base64.URLEncoding.EncodeToString(data), "=")
 }
 
+// Decode websafe-base64 string without '=' padding
 func Decode(data string) ([]byte, error) {
 
 	// Websafe base64 doesn't have padding, append it (len should be %4)
